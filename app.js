1c919fd819dac9917b968923dd3ce9cc08fa151d
Basanta-Web <basantarockstar60@gmail.com>
2026-03-09 01:16:25 +0530

    app.js

--- app.js
+++ app.js
@@ -1,7 +1,52 @@
-function reloadSheet(){
-location.reload()
+const excelFile = "Truesped_Broadband_2026 5.xlsx"
+
+let tableData = []
+
+async function loadExcel(){
+
+const response = await fetch(excelFile)
+
+const buffer = await response.arrayBuffer()
+
+const workbook = XLSX.read(buffer,{type:"array"})
+
+const sheet = workbook.Sheets[workbook.SheetNames[0]]
+
+tableData = XLSX.utils.sheet_to_json(sheet,{header:1})
+
+renderTable()
+
 }
 
-setInterval(function(){
-location.reload()
-},300000)
+function renderTable(){
+
+const container = document.getElementById("table")
+
+HyperFormula.buildFromArray(tableData)
+
+new Handsontable(container,{
+
+data:tableData,
+rowHeaders:true,
+colHeaders:true,
+stretchH:"all",
+height:"100%",
+licenseKey:"non-commercial-and-evaluation"
+
+})
+
+}
+
+function backupExcel(){
+
+const ws = XLSX.utils.aoa_to_sheet(tableData)
+
+const wb = XLSX.utils.book_new()
+
+XLSX.utils.book_append_sheet(wb,ws,"Customers")
+
+XLSX.writeFile(wb,"backup.xlsx")
+
+}
+
+loadExcel()
